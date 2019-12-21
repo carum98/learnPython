86c2f7eedf249a62e55e732200bbf9eb8fe82be0
Carlos Umaña Acevedo <carumtito@gmail.com>
2019-12-20 18:01:22 -0600

    Tipos de datos

---
+++ tipos_de_variables.py
@@ -0,0 +1,25 @@
+# entero
+entero = 10
+
+# flotantes
+flotantes = 10.05
+
+# complejos
+complejo = (10.05+0j)
+
+print(type(flotantes))
+print(int(flotantes))
+print(flotantes)
+
+# booleanos
+verdadero = True
+falso = False
+
+print(bool(verdadero))
+
+# cadena de texto
+cadena = 'Carlos Umaña'
+cadena1 = "Carlos Umaña"
+str(entero)
+
+print(cadena)
