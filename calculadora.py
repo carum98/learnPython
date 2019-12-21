0d2ba9088b73e4aa4471952dd42d6d47c7ab0d00
Carlos Umaña Acevedo <carumtito@gmail.com>
2019-12-20 18:51:44 -0600

    Calculadora simple

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+num1 = input("Ingrese el primer numero: ")
+num2 = input("Ingrese el segundo numero: ")
+
+sum = float(num1)+float(num2)
+res = float(num1)-float(num2)
+mul = float(num1)*float(num2)
+print("La suma es:", sum)
+print("La resta es:", res)
+print("La multiplicacion es:", mul)
